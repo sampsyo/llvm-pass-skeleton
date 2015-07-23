1c3b576eb00f2671d944bb765e1ba790966a3cf1
Adrian Sampson <adrian@radbox.org>
2015-07-23 11:52:21 -0700

    Iterators

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -11,7 +11,21 @@ namespace {
     SkeletonPass() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
+      errs() << "In a function called " << F.getName() << "!\n";
+
+      errs() << "Function body:\n";
+      F.dump();
+
+      for (auto &B : F) {
+        errs() << "Basic block:\n";
+        B.dump();
+
+        for (auto &I : B) {
+          errs() << "Instruction: ";
+          I.dump();
+        }
+      }
+
       return false;
     }
   };
