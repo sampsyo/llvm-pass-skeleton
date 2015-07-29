721732ce9620ca200a436397506d735077ac6f50
Adrian Sampson <adrian@radbox.org>
2015-07-29 02:00:18 -0700

    Slightly less trivial mutation

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -4,6 +4,8 @@
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
 using namespace llvm;
 
 namespace {
@@ -12,17 +14,31 @@ namespace {
     SkeletonPass() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
-      bool changed = false;
       for (auto &B : F) {
         for (auto &I : B) {
           if (auto *op = dyn_cast<BinaryOperator>(&I)) {
-            op->swapOperands();
-            changed = true;
+            // Insert at the point where the instruction `op` appears.
+            IRBuilder<> builder(op);
+
+            // Make a multiply with the same operands as `op`.
+            Value *lhs = op->getOperand(0);
+            Value *rhs = op->getOperand(1);
+            Value *mul = builder.CreateMul(lhs, rhs);
+
+            // Everywhere the old instruction was used as an operand, use our
+            // new multiply instruction instead.
+            for (auto &U : op->uses()) {
+              User *user = U.getUser();
+              user->setOperand(U.getOperandNo(), mul);
+            }
+
+            // We modified the code.
+            return true;
           }
         }
       }
 
-      return changed;
+      return false;
     }
   };
 }
