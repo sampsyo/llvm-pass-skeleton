aa62487836ff287d246461ac4cb4b6c38fe36697
LLVM Student <llvm@example.com>
2017-11-22 21:47:06 +0100

    Minor tweaks to skeleton
    * Update CMake version to 3.4.3.
    * Use override instead of virtual
    * Register the pass so opt can see it

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -3,6 +3,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
 using namespace llvm;
 
 namespace {
@@ -10,7 +11,7 @@ namespace {
     static char ID;
     SkeletonPass() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
+    bool runOnFunction(Function &F) override {
       errs() << "I saw a function called " << F.getName() << "!\n";
       return false;
     }
@@ -19,12 +20,16 @@ namespace {
 
 char SkeletonPass::ID = 0;
 
-// Automatically enable the pass.
+// Make the pass visible to opt.
+static RegisterPass<SkeletonPass> X("skeleton", "Skeleton Pass");
+
+// Automatically enable the pass in clang.
 // http://adriansampson.net/blog/clangpass.html
 static void registerSkeletonPass(const PassManagerBuilder &,
                          legacy::PassManagerBase &PM) {
   PM.add(new SkeletonPass());
 }
+
 static RegisterStandardPasses
   RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
                  registerSkeletonPass);
