f947eb6e3c703fd2baa3f09fcdbd1a7066c30657
Adrian Sampson <adrian@radbox.org>
2023-09-25 22:39:39 -0400

    Modernize

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -1,56 +1,57 @@
 #include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/IR/InstrTypes.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 using namespace llvm;
 
 namespace {
-  struct SkeletonPass : public FunctionPass {
-    static char ID;
-    SkeletonPass() : FunctionPass(ID) {}
 
-    virtual bool runOnFunction(Function &F) {
-      for (auto &B : F) {
-        for (auto &I : B) {
-          if (auto *op = dyn_cast<BinaryOperator>(&I)) {
-            // Insert at the point where the instruction `op` appears.
-            IRBuilder<> builder(op);
+struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
+    PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+        for (auto &F : M.functions()) {
+            for (auto &B : F) {
+                for (auto &I : B) {
+                    if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+                        // Insert at the point where the instruction `op` appears.
+                        IRBuilder<> builder(op);
 
-            // Make a multiply with the same operands as `op`.
-            Value *lhs = op->getOperand(0);
-            Value *rhs = op->getOperand(1);
-            Value *mul = builder.CreateMul(lhs, rhs);
+                        // Make a multiply with the same operands as `op`.
+                        Value *lhs = op->getOperand(0);
+                        Value *rhs = op->getOperand(1);
+                        Value *mul = builder.CreateMul(lhs, rhs);
 
-            // Everywhere the old instruction was used as an operand, use our
-            // new multiply instruction instead.
-            for (auto &U : op->uses()) {
-              User *user = U.getUser();  // A User is anything with operands.
-              user->setOperand(U.getOperandNo(), mul);
+                        // Everywhere the old instruction was used as an operand, use our
+                        // new multiply instruction instead.
+                        for (auto &U : op->uses()) {
+                          User *user = U.getUser();  // A User is anything with operands.
+                          user->setOperand(U.getOperandNo(), mul);
+                        }
+
+                        // We modified the code.
+                        return PreservedAnalyses::none();
+                    }
+                }
             }
-
-            // We modified the code.
-            return true;
-          }
         }
-      }
+        return PreservedAnalyses::all();
+    };
+};
 
-      return false;
-    }
-  };
 }
 
-char SkeletonPass::ID = 0;
-
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
-static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new SkeletonPass());
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+    return {
+        .APIVersion = LLVM_PLUGIN_API_VERSION,
+        .PluginName = "Skeleton pass",
+        .PluginVersion = "v0.1",
+        .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
+            PB.registerPipelineStartEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel Level) {
+                    MPM.addPass(SkeletonPass());
+                });
+        }
+    };
 }
-static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
