d7eaf3d30434b8f3da9a26f9c5d8544d65da9026
Adrian Sampson <adrian@radbox.org>
2023-09-25 22:33:55 -0400

    Start revision

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -1,46 +1,58 @@
 #include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/LegacyPassManager.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
 using namespace llvm;
 
 namespace {
+
+struct SkeletonPass : public PassInfoMixin<SkeletonPass> {
+    PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+        for (auto &F : M.functions()) {
+            errs() << "In a function called " << F.getName() << "!\n";
+
+            errs() << "Function body:\n";
+            F.print(errs());
+
+            for (auto &B : F) {
+              errs() << "Basic block:\n";
+              B.print(errs());
+
+              for (auto &I : B) {
+                errs() << "Instruction: \n";
+                I.print(errs(), true);
+                errs() << "\n";
+              }
+            }
+            errs() << "I saw a function called " << F.getName() << "!\n";
+        }
+        return PreservedAnalyses::all();
+    };
+};
+
   struct SkeletonPass : public FunctionPass {
     static char ID;
     SkeletonPass() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
-      errs() << "In a function called " << F.getName() << "!\n";
-
-      errs() << "Function body:\n";
-      //F.dump(); Out-of-Date: no more dump support in modern llvm unless you enable it at compile time.
-      F.print(llvm::errs());
-
-      for (auto &B : F) {
-        errs() << "Basic block:\n";
-        B.print(llvm::errs());
-
-        for (auto &I : B) {
-          errs() << "Instruction: \n";
-          I.print(llvm::errs(), true);
-          errs() << "\n";
-        }
-      }
 
       return false;
     }
   };
 }
 
-char SkeletonPass::ID = 0;
-
-// Automatically enable the pass.
-// http://adriansampson.net/blog/clangpass.html
-static void registerSkeletonPass(const PassManagerBuilder &,
-                         legacy::PassManagerBase &PM) {
-  PM.add(new SkeletonPass());
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+    return {
+        .APIVersion = LLVM_PLUGIN_API_VERSION,
+        .PluginName = "Skeleton pass",
+        .PluginVersion = "v0.1",
+        .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
+            PB.registerPipelineStartEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel Level) {
+                    MPM.addPass(SkeletonPass());
+                });
+        }
+    };
 }
-static RegisterStandardPasses
-  RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-                 registerSkeletonPass);
